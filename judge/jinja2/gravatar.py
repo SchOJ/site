57b4712290b2cc975a8533dfb98f8f8a462a222e
Outvi V <git@outv.im>
2019-01-04 16:30:24 +0800

    gravatar -> loli.net

--- judge/jinja2/gravatar.py
+++ judge/jinja2/gravatar.py
@@ -16,7 +16,7 @@ def gravatar(email, size=80, default=None):
     elif isinstance(email, AbstractUser):
         email = email.email
 
-    gravatar_url = '//www.gravatar.com/avatar/' + hashlib.md5(email.strip().lower()).hexdigest() + '?'
+    gravatar_url = '//gravatar.loli.net/avatar/' + hashlib.md5(email.strip().lower()).hexdigest() + '?'
     args = {'d': 'identicon', 's': str(size)}
     if default:
         args['f'] = 'y'
