b6041a5472ec4ead4e00f0cdd881bc30452a43d1
schrodingerzhu <jimmyzhu1999@protonmail.com>
2019-01-12 15:11:33 +0800

    Fix Query Hack
    Co-authored-by: Outvi V <git@outv.im>

--- judge/utils/raw_sql.py
+++ judge/utils/raw_sql.py
@@ -39,8 +39,13 @@ def RawSQLColumn(model, field=None):
 
 def make_straight_join_query(QueryType):
     class Query(QueryType):
-        def join(self, join, reuse=None):
-            alias = super(Query, self).join(join, reuse)
+        def __init__(self):
+            """
+            This class can only be created from factory function
+            """
+
+        def join(self, join, reuse=None, reuse_with_filtered_relation=False):
+            alias = super(Query, self).join(join, reuse, reuse_with_filtered_relation)
             join = self.alias_map[alias]
             if join.join_type == INNER:
                 join.join_type = 'STRAIGHT_JOIN'
@@ -52,10 +57,53 @@ def make_straight_join_query(QueryType):
 straight_join_cache = CacheDict(make_straight_join_query)
 
 
+def clone_to_new(old_query):
+    """
+    Return a copy of the current Query. A lightweight alternative to
+    to deepcopy().
+    """
+    obj = straight_join_cache[type(old_query)]()
+    # Copy references to everything.
+    obj.__dict__ = old_query.__dict__.copy()
+    # Clone attributes that can't use shallow copy.
+    obj.alias_refcount = old_query.alias_refcount.copy()
+    obj.alias_map = old_query.alias_map.copy()
+    obj.external_aliases = old_query.external_aliases.copy()
+    obj.table_map = old_query.table_map.copy()
+    obj.where = old_query.where.clone()
+    obj._annotations = old_query._annotations.copy() if old_query._annotations is not None else None
+    if old_query.annotation_select_mask is None:
+        obj.annotation_select_mask = None
+    else:
+        obj.annotation_select_mask = old_query.annotation_select_mask.copy()
+    # _annotation_select_cache cannot be copied, as doing so breaks the
+    # (necessary) state in which both annotations and
+    # _annotation_select_cache point to the same underlying objects.
+    # It will get re-populated in the cloned queryset the next time it's
+    # used.
+    obj._annotation_select_cache = None
+    obj._extra = old_query._extra.copy() if old_query._extra is not None else None
+    if old_query.extra_select_mask is None:
+        obj.extra_select_mask = None
+    else:
+        obj.extra_select_mask = old_query.extra_select_mask.copy()
+    if old_query._extra_select_cache is None:
+        obj._extra_select_cache = None
+    else:
+        obj._extra_select_cache = old_query._extra_select_cache.copy()
+    if 'subq_aliases' in old_query.__dict__:
+        obj.subq_aliases = old_query.subq_aliases.copy()
+    obj.used_aliases = old_query.used_aliases.copy()
+    obj._filtered_relations = old_query._filtered_relations.copy()
+    # Clear the cached_property
+    try:
+        del obj.base_table
+    except AttributeError:
+        pass
+    return obj
+
+
 def use_straight_join(queryset):
     if connections[queryset.db].vendor != 'mysql':
         return
-    # TODO: Understand what is this
-    x = queryset.query.clone()
-    x.__dict__.update(straight_join_cache)
-    queryset.query = x
+    queryset.query = clone_to_new(queryset.query)
